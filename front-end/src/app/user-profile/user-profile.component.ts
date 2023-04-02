5a77459d21a19548ba14d3136518c2a91c37e2c9
Sarthak Pokharel <sarthakpokharel@gmail.com>
2023-04-02 11:35:48 +0545

    updates

--- front-end/src/app/user-profile/user-profile.component.ts
+++ front-end/src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../auth/auth.service';
+import { UserProfileService } from './user-profile.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+
+  formlayout: any;
+
+  passwordForm: any;
+
+  model: any = {};
+
+  username ="";
+
+  constructor(private fb: FormBuilder, private RS: UserProfileService, private auth: AuthService, private toastr: ToastrService){
+    this.formlayout = 
+      {
+        oldpassword: ['', Validators.required],
+        password: ['', Validators.required],
+        confirmpassword: ['', Validators.required]
+      }
+
+      this.passwordForm =fb.group(this.formlayout);
+
+      const details = auth.getUserDetails();
+      if (details) {
+        this.username = details.username;
+      }
+    
+  }
+
+        
+      
+  ngOnInit(): void{
+
+    this.getUser(this.username);
+
+  }
+
+  userDetails : any;
+
+  getUser(username: any){
+    this.RS.getDetails(username).subscribe({next:(dt)=>{
+      this.userDetails = dt;
+      console.log(this.userDetails)
+    },error:error=>{
+      this.toastr.error("error.error.message");
+    }});
+  }
+
+  passwordFormSubmit(){
+    if (this.passwordForm.valid){
+      this.model = this.passwordForm.value;
+      this.createItem(6); //add id here
+    }
+  }
+
+  createItem(id: any){
+
+  }
 
 }
---
+++ front-end/src/app/user-profile/user-profile.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from '../api.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserProfileService {
+  
+  url="users";
+
+  update(id: any, data: any) {
+    return this.http.put(this.url + '/' + id, data);
+  }
+
+  constructor(private http: ApiService) { 
+  }
+
+getDetails(username: string) {
+  return this.http.get(this.url + '/get-user-details' + "?username="+username);
+
+}
+
+}
