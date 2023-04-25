79ce87c40d0f407b4a44fe6d62fb7e322d1e14f4
khagendra <khagendra@saipal.org>
2023-04-25 08:16:41 +0545

    cheque

--- src/main/java/org/saipal/srms/vouchers/BankVoucherService.java
+++ src/main/java/org/saipal/srms/vouchers/BankVoucherService.java
@@ -75,8 +75,8 @@ public class BankVoucherService extends AutoService {
 			String usq="select * from chequeBankDakhilaDetail where did=?";	
 			Tuple res = db.getSingleResult(usq, Arrays.asList(jarr.get(i)));
 			
-			String sql="insert into taxvouchers(id,bankid,karobarsanket,chequeno,chequeamount,cstatus,chequebank,lgid,date,taxpayername,bankorgid,amountcr,cstatus) values(?,?,?,?,?,?,?,?,?,?,?)";
-			db.execute(sql,Arrays.asList(res.get("did"),res.get("bankid"),res.get("ksno"),res.get("chequeno"),res.get("chequeamount"),res.get("cstatus")));
+			String sql="insert into taxvouchers(id,bankid,karobarsanket,chequeno,chequeamount,cstatus,chequebank,lgid,date,taxpayername,bankorgid,amountcr,cstatus,ttype) values(?,?,?,?,?,?,?,?,?,?,?,?)";
+			db.execute(sql,Arrays.asList(res.get("did"),res.get("bankid"),res.get("ksno"),res.get("chequeno"),res.get("chequeamount"),0,2));
 			}
 		}
 		return null;
@@ -176,7 +176,6 @@ public class BankVoucherService extends AutoService {
 		Map<String, Object> data = db.getSingleResultMap(sql, Arrays.asList(transactionid,auth.getBankId()));
 		if(data==null) {
 			JSONObject dt =  api.getChequeDetails(transactionid);
-			System.out.println(dt.toString());
 			if(dt!=null) {
 				try {
 					if(dt.getInt("status")==1) {
@@ -184,7 +183,7 @@ public class BankVoucherService extends AutoService {
 						JSONObject d = dt.getJSONObject("data");
 						DbResponse rf= db.execute("insert into chequeBankDakhilaMain (cdid ,adminid ,orgid ,fyid ,trantype ,karobarSanketNo ,trandate ,trandatetint ,refNo ,narration ,bankorgid ,bankid ,accountno ,entrydate ,enterby) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
 								Arrays.asList(d.get("cdid"),d.get("adminid"),d.get("orgid"),d.get("fyid"),d.get("trantype"),d.get("karobarsanketno"),d.get("trandate"),d.get("trandatetint"),d.get("refno"),d.get("narration"),d.get("bankorgid"),d.get("bankid"),d.get("accountno"),d.get("entrydate"),d.get("enterby")));
-						System.out.println("first if");
+						
 						 JSONArray dtls=d.getJSONArray("details_rows");
 						
 						if(dtls.length()>0) {
@@ -192,7 +191,7 @@ public class BankVoucherService extends AutoService {
 								JSONObject dd = dtls.getJSONObject(i);
 								db.execute("insert into chequeBankDakhilaDetail(did ,mainid ,rcid ,ksno ,bankid ,chequeno ,chequeamount ,taxpayername ) values(?,?,?,?,?,?,?,?)",
 										Arrays.asList(dd.get("did"),dd.get("mainid"),dd.get("rcid"),dd.get("ksno"),dd.get("bankid"),dd.get("chequeno"),dd.get("chequeamount"),dd.get("taxpayername")));
-								System.out.println("loop");
+							
 							}
 						}
 						sql = "select bd.fyid,bd.trantype,bd.cdid,bd.karobarSanketNo,bd.orgid as lgid,bd.trandate,bd.trandatetint,bd.bankid,bd.accountno,ba.accountname,bi.namenp as bankname,ll.namenp as palika from chequeBankDakhilaMain bd join bankaccount ba on ba.id=bd.bankorgid join bankinfo bi on bi.id=bd.bankid join admin_local_level_structure ll on ll.id=bd.orgid where karobarSanketNo=? and bd.bankid=?";
