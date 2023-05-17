38c23f719d6b5d1c6061921986b8413dd967cc90
Sarthak Pokharel <sarthakpokharel@gmail.com>
2023-05-17 13:31:53 +0545

    settings Update

---
+++ src/main/java/org/saipal/srms/settings/SettingsController.java
@@ -0,0 +1,24 @@
+package org.saipal.srms.settings;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/settings")
+public class SettingsController {
+	@Autowired
+	SettingsService objService;
+	
+	@PostMapping("otp-settings")
+	public ResponseEntity<Map<String, Object>> OtpSettings(HttpServletRequest request) {
+			return objService.changeOtpSettings();
+	}
+}
---
+++ src/main/java/org/saipal/srms/settings/SettingsService.java
@@ -0,0 +1,12 @@
+package org.saipal.srms.settings;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+
+public class SettingsService {
+	public ResponseEntity<Map<String, Object>> changeOtpSettings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
