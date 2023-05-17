8f8854d140d4a9276846e42415954c2a0220e3e2
Sarthak Pokharel <sarthakpokharel@gmail.com>
2023-05-17 13:36:25 +0545

    component annotation added

--- src/main/java/org/saipal/srms/settings/SettingsService.java
+++ src/main/java/org/saipal/srms/settings/SettingsService.java
@@ -3,7 +3,9 @@ package org.saipal.srms.settings;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SettingsService {
 	public ResponseEntity<Map<String, Object>> changeOtpSettings() {
 		// TODO Auto-generated method stub
