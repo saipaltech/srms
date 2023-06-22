c9f9ce9617b99be46e5dd56913f1a0d7f3645c1c
unknown <bhimpdsharma@gmail.com>
2023-06-22 17:53:27 +0545

    added schedular

---
+++ src/main/java/org/saipal/srms/TaskRunner.java
@@ -0,0 +1,42 @@
+package org.saipal.srms;
+
+import org.codehaus.jettison.json.JSONException;
+import org.saipal.srms.dayclose.DaycloseService;
+import org.saipal.srms.util.DB;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TaskRunner {
+	
+	Logger log = LoggerFactory.getLogger(TaskRunner.class);
+	
+	@Autowired
+	DB db;
+	
+	@Autowired
+	DaycloseService ds;
+	
+	@Scheduled(cron="0 5 0 * * *")
+	public void deleteAllUnVerifiedVouchers() {
+		log.info("Auto Delete Unverified Vouchers Strted");
+		db.execute("delete from taxvouchers where dateint < (cast(format(getdate(),'yyyyMMdd') as int)) and ttype=1 and approved=0");
+		log.info("Auto Delete Unverified Vouchers End");
+	}
+	
+	@Scheduled(cron="0 55 23 * * *")
+	public void automaticDayClose() {
+		log.info("Automatic DayClose Started");
+		try {
+			ds.daycloseScheduler();
+			log.info("Automatic Dayclose Done");
+		} catch (JSONException e) {
+			log.error(e.getMessage());
+		}
+		log.info("Automatic Dayclose End");
+	}
+}
